fb28bca6e7cbd95c6f41a80fa3b99491354463f9
Athulrr <98388862+Athulrr@users.noreply.github.com>
2024-12-13 21:28:26 +0530

    Create SplashScreen.jsx

---
+++ SplashScreen.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import Lottie from 'react-lottie';
+import { useNavigate } from 'react-router-dom';
+import splashAnimation from '../assets/1.json'; // Correct the relative path
+// import './SplashScreen.css';
+
+function SplashScreen() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/home');  // Redirect to home after 3 seconds
+    }, 3000);  // Adjust duration as needed
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  const defaultOptions = {
+    loop: false, // Animation will play once
+    autoplay: true, // Animation will start automatically
+    animationData: splashAnimation, // Your Lottie animation data
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return (
+    <div className="splash-screen">
+      <Lottie options={defaultOptions} height={400} width={400} />
+     
+    </div>
+  );
+}
+
+export default SplashScreen;
