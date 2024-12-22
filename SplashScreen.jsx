1e930f4f3ced3c52cb7b8e40a6eaac6606537fc9
Adarsh CP <119414032+Cpa02@users.noreply.github.com>
2024-12-22 22:21:45 +0530

    Update SplashScreen.jsx

--- SplashScreen.jsx
+++ SplashScreen.jsx
@@ -1,15 +1,21 @@
+// Note:This Is To Be Installed In Terminal
+//npm install react-lottie
+// npm install lottie-web
+
+
 import React, { useEffect } from 'react';
-import Lottie from 'react-lottie';
+import Lottie from "react-lottie";
 import { useNavigate } from 'react-router-dom';
 import splashAnimation from '../assets/2.json'; // Correct the relative path
+// import './SplashScreen.css';
 
 function SplashScreen() {
   const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate('/home'); // Redirect to home after 3 seconds
-    }, 3000); // Adjust duration as needed
+      navigate('/home');  // Redirect to home after 3 seconds
+    }, 3000);  // Adjust duration as needed
     return () => clearTimeout(timer);
   }, [navigate]);
 
@@ -24,13 +30,10 @@ function SplashScreen() {
 
   return (
     <div className="splash-screen">
-      <div className="lottie-container">
-        <Lottie options={defaultOptions} />
-      </div>
+      <Lottie options={defaultOptions} height={400} width={400} />
+     
     </div>
   );
 }
 
 export default SplashScreen;
-
-
